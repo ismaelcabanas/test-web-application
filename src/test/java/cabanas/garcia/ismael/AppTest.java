d42c06677f71ade420ee36a96b176193af7fb6b6
XI317311 <ismael.cabanas@extern.isban.com>
2017-02-24 16:14:37 +0100

    First test class to check build

---
+++ src/test/java/cabanas/garcia/ismael/AppTest.java
@@ -0,0 +1,15 @@
+package cabanas.garcia.ismael;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by XI317311 on 24/02/2017.
+ */
+public class AppTest {
+
+    @Test
+    public void testIt(){
+        Assert.assertTrue(true);
+    }
+}
