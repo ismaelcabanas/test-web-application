32b469c760ff86f6417cd8188686b4df439594b9
XI317311 <ismael.cabanas@extern.isban.com>
2017-03-01 09:18:42 +0100

    Refactor test

--- src/test/java/cabanas/garcia/ismael/opportunity/server/StandardWebServerTest.java
+++ src/test/java/cabanas/garcia/ismael/opportunity/server/StandardWebServerTest.java
@@ -2,31 +2,41 @@ package cabanas.garcia.ismael.opportunity.server;
 
 import org.hamcrest.core.Is;
 import org.hamcrest.core.IsEqual;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class StandardWebServerTest {
 
+    private int port;
+    private WebServer standardWebServer;
+
+    @Before
+    public void setUp() throws Exception {
+        port = 8003;
+        standardWebServer = new StandardWebServer(port);
+    }
+
+    @After
+    public void tearDown(){
+        if(standardWebServer.isRunning()){
+            standardWebServer.stop();
+        }
+    }
+
     @Test
     public void when_i_start_server_on_a_port_then_the_server_is_up(){
-        // given
-        int port = 8002;
-        WebServer standardWebServer = new StandardWebServer(port);
-
         // when
         standardWebServer.start();
 
         // then
         Assert.assertThat(standardWebServer.isRunning(), Is.is(IsEqual.equalTo(true)));
-
-        standardWebServer.stop();
     }
 
     @Test
     public void when_i_stop_a_started_server_then_the_server_is_down(){
         // given
-        int port = 8002;
-        WebServer standardWebServer = new StandardWebServer(port);
         standardWebServer.start();
 
         // when
@@ -38,10 +48,6 @@ public class StandardWebServerTest {
 
     @Test
     public void if_the_server_is_not_started_the_server_is_down(){
-        // given
-        int port = 8002;
-        WebServer standardWebServer = new StandardWebServer(port);
-
         // then
         Assert.assertThat(standardWebServer.isRunning(), Is.is(IsEqual.equalTo(false)));
     }
