cb618d4919037dc60b3c7c7915b9d8febd1a36c5
XI317311 <ismael.cabanas@extern.isban.com>
2017-03-07 19:35:47 +0100

    Sonar bug fixed

--- src/main/java/cabanas/garcia/ismael/opportunity/internal/creation/instance/ConstructorInstantiator.java
+++ src/main/java/cabanas/garcia/ismael/opportunity/internal/creation/instance/ConstructorInstantiator.java
@@ -1,12 +1,17 @@
 package cabanas.garcia.ismael.opportunity.internal.creation.instance;
 
+import java.lang.*;
+
 public class ConstructorInstantiator implements Instantiator {
     @Override
     public <T> T newInstance(Class<T> aClass) {
         try {
             return aClass.newInstance();
-        } catch (Throwable e) {
+        } catch (java.lang.InstantiationException e) {
+            throw new InstantiationException("Unable to create instance of \'" + aClass.getSimpleName() + "\'.\nPlease ensure it has 0-arg constructor which invokes cleanly.", e);
+        } catch (IllegalAccessException e) {
             throw new InstantiationException("Unable to create instance of \'" + aClass.getSimpleName() + "\'.\nPlease ensure it has 0-arg constructor which invokes cleanly.", e);
         }
+
     }
 }
