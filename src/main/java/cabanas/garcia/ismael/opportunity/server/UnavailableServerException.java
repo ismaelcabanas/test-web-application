05689414f5cdf69d324a88edabe9966d5d4299ab
XI317311 <ismael.cabanas@extern.isban.com>
2017-03-07 18:52:44 +0100

    Converted to unchecked exception

--- src/main/java/cabanas/garcia/ismael/opportunity/server/UnavailableServerException.java
+++ src/main/java/cabanas/garcia/ismael/opportunity/server/UnavailableServerException.java
@@ -2,10 +2,7 @@ package cabanas.garcia.ismael.opportunity.server;
 
 import java.io.IOException;
 
-/**
- *
- */
-public class UnavailableServerException extends Exception {
+public class UnavailableServerException extends RuntimeException {
     public UnavailableServerException(IOException e) {
         super(e);
     }
