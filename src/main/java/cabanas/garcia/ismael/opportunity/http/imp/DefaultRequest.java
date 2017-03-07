40a04d33110eb27e9e12e0677f3270f5449ca53e
XI317311 <ismael.cabanas@extern.isban.com>
2017-03-07 19:21:11 +0100

    Added toString()

--- src/main/java/cabanas/garcia/ismael/opportunity/http/imp/DefaultRequest.java
+++ src/main/java/cabanas/garcia/ismael/opportunity/http/imp/DefaultRequest.java
@@ -3,9 +3,11 @@ package cabanas.garcia.ismael.opportunity.http.imp;
 import cabanas.garcia.ismael.opportunity.http.Request;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 @Builder
 @EqualsAndHashCode
+@ToString
 public class DefaultRequest implements Request{
     private String path;
 
