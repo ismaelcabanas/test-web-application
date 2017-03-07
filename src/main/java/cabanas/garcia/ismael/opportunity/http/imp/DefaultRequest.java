d97e9e669054f37d8f2c3d1705f963907bafa723
XI317311 <ismael.cabanas@extern.isban.com>
2017-03-07 23:54:12 +0100

    Add @Getter lombok annotation

--- src/main/java/cabanas/garcia/ismael/opportunity/http/imp/DefaultRequest.java
+++ src/main/java/cabanas/garcia/ismael/opportunity/http/imp/DefaultRequest.java
@@ -3,16 +3,13 @@ package cabanas.garcia.ismael.opportunity.http.imp;
 import cabanas.garcia.ismael.opportunity.http.Request;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.ToString;
 
 @Builder
 @EqualsAndHashCode
 @ToString
+@Getter
 public class DefaultRequest implements Request{
     private String path;
-
-    @Override
-    public String getPath() {
-        return path;
-    }
 }
