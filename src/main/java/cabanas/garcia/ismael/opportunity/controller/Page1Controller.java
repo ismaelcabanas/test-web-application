c0fd21159e416579c0f29f2607277969bef7bc2f
XI317311 <ismael.cabanas@extern.isban.com>
2017-03-07 18:40:11 +0100

    First version of Page1Controller

---
+++ src/main/java/cabanas/garcia/ismael/opportunity/controller/Page1Controller.java
@@ -0,0 +1,17 @@
+package cabanas.garcia.ismael.opportunity.controller;
+
+import cabanas.garcia.ismael.opportunity.http.Request;
+import cabanas.garcia.ismael.opportunity.view.Page1RawView;
+import cabanas.garcia.ismael.opportunity.view.View;
+
+public class Page1Controller extends Controller{
+    @Override
+    public View process(Request request) {
+        return new Page1RawView();
+    }
+
+    @Override
+    public String getMappingPath() {
+        return "/page1";
+    }
+}
