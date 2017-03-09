a40c63cea8607c1ec1b6bbf0fd90063225ab23ea
XI317311 <ismael.cabanas@extern.isban.com>
2017-03-09 16:29:12 +0100

    Controller and view for login GET requests

---
+++ src/main/java/cabanas/garcia/ismael/opportunity/controller/LoginController.java
@@ -0,0 +1,17 @@
+package cabanas.garcia.ismael.opportunity.controller;
+
+import cabanas.garcia.ismael.opportunity.http.Request;
+import cabanas.garcia.ismael.opportunity.view.LoginRawView;
+import cabanas.garcia.ismael.opportunity.view.View;
+
+public class LoginController implements Controller{
+    @Override
+    public View process(Request request) {
+        return new LoginRawView();
+    }
+
+    @Override
+    public String getMappingPath() {
+        return "/login";
+    }
+}
--- src/main/java/cabanas/garcia/ismael/opportunity/view/LoginRawView.java
+++ src/main/java/cabanas/garcia/ismael/opportunity/view/LoginRawView.java
@@ -6,11 +6,25 @@ import cabanas.garcia.ismael.opportunity.http.imp.DefaultResponse;
 import java.net.HttpURLConnection;
 
 public class LoginRawView implements View{
+
+    private static final String raw = "<html>\n" +
+            "\t<body>\n" +
+            "\t<h1>Login</h1>\n" +
+            "\t<p>Introduce your credentials</p>\t\n" +
+            "\t<form action=\"/login\" method=\"post\">\n" +
+            "\t\t<label for=\"user\">User</label>\n" +
+            "\t\t<input type=\"text\" name=\"user\">\n" +
+            "\t\t<label for=\"password\">Password</label>\n" +
+            "\t\t<input type=\"password\" name=\"password\">\n" +
+            "\t\t<input type=\"submit\" value=\"Login\">\n" +
+            "\t</form>\n" +
+            "\t</body>\n" +
+            "</html>";
     @Override
     public Response render() {
         return DefaultResponse.builder()
                 .statusCode(HttpURLConnection.HTTP_OK)
-                .content("Login".getBytes())
+                .content(raw.getBytes())
                 .build();
     }
 }
