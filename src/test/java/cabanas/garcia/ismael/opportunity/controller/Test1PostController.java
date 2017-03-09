fbd9e91df0af740bd684e67403df94b1cf9b6e77
XI317311 <ismael.cabanas@extern.isban.com>
2017-03-09 19:34:22 +0100

    New test controller for POST request

---
+++ src/test/java/cabanas/garcia/ismael/opportunity/controller/Test1PostController.java
@@ -0,0 +1,32 @@
+package cabanas.garcia.ismael.opportunity.controller;
+
+import cabanas.garcia.ismael.opportunity.http.Request;
+import cabanas.garcia.ismael.opportunity.http.RequestMethodConstants;
+import cabanas.garcia.ismael.opportunity.view.View;
+
+public class Test1PostController extends AbstractController {
+    private String path;
+
+    public Test1PostController() {
+    }
+
+    @Override
+    public View process(Request request) {
+        return null;
+    }
+
+    @Override
+    public String getMappingPath() {
+        return "/test1";
+    }
+
+    public Test1PostController(String path) {
+        super();
+        this.path = path;
+    }
+
+    @Override
+    public String getMethod() {
+        return RequestMethodConstants.POST;
+    }
+}
