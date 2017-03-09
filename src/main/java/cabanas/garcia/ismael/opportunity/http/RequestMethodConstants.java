9716fce2014829c0bb59f9b9bff0be59edaba339
XI317311 <ismael.cabanas@extern.isban.com>
2017-03-09 16:51:41 +0100

    Added request methods constants

---
+++ src/main/java/cabanas/garcia/ismael/opportunity/http/RequestMethodConstants.java
@@ -0,0 +1,8 @@
+package cabanas.garcia.ismael.opportunity.http;
+
+public final class RequestMethodConstants {
+    public static final String POST = "POST";
+    public static final String GET = "GET";
+
+    private RequestMethodConstants(){}
+}
