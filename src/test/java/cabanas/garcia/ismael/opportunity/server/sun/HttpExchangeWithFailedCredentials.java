760ee4fe7374a7b69169fc75e2008028c9e6c842
XI317311 <ismael.cabanas@extern.isban.com>
2017-03-09 21:23:04 +0100

    Added new stub for HttpExchange

---
+++ src/test/java/cabanas/garcia/ismael/opportunity/server/sun/HttpExchangeWithFailedCredentials.java
@@ -0,0 +1,29 @@
+package cabanas.garcia.ismael.opportunity.server.sun;
+
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.net.URI;
+
+
+public class HttpExchangeWithFailedCredentials extends HttpExchangeStub {
+    private static final String payload = "username=ismael&" +
+            "password=failed";
+
+    public HttpExchangeWithFailedCredentials() {
+        super();
+        getRequestHeaders().add("Content-type", "application/x-www-form-urlencoded");
+    }
+
+    @Override
+    public InputStream getRequestBody() {
+        InputStream is = new ByteArrayInputStream(payload.getBytes());
+        return is;
+    }
+
+    @Override
+    public URI getRequestURI() {
+        return URI.create("/test");
+    }
+}
