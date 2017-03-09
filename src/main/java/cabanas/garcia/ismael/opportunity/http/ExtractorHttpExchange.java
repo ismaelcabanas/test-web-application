21c87e5b62349f9a2409db901230add0a10a7b60
XI317311 <ismael.cabanas@extern.isban.com>
2017-03-09 21:20:37 +0100

    Extract parameters from HttpExchange

--- src/main/java/cabanas/garcia/ismael/opportunity/http/ExtractorHttpExchange.java
+++ src/main/java/cabanas/garcia/ismael/opportunity/http/ExtractorHttpExchange.java
@@ -6,8 +6,10 @@ import cabanas.garcia.ismael.opportunity.http.cookies.Cookies;
 import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 
-import java.util.List;
-import java.util.Optional;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
 
 public class ExtractorHttpExchange {
     private final HttpExchange httpExchange;
@@ -33,5 +35,48 @@ public class ExtractorHttpExchange {
             }
         }
         return Optional.empty();
+    }
+
+    public List<Parameter> getParameters() {
+        List<Parameter> parameters = new ArrayList<>();
+
+        String qry = "";
+        InputStream in = httpExchange.getRequestBody();
+        try {
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            byte buf[] = new byte[4096];
+            for (int n = in.read(buf); n > 0; n = in.read(buf)) {
+                out.write(buf, 0, n);
+            }
+            qry = new String(out.toByteArray());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        // parse the request body
+        Map<String,List<String>> parms = new HashMap<>();
+        String defs[] = qry.split("[&]");
+        for (String def: defs) {
+            int ix = def.indexOf('=');
+            String name;
+            String value;
+            Parameter parameter = null;
+            if (ix < 0) {
+                parameter = Parameter.builder().name(def).value("").build();
+            } else {
+                parameter = Parameter.builder().name(def.substring(0, ix)).value(def.substring(ix+1)).build();
+            }
+            parameters.add(parameter);
+        }
+        return parameters;
+    }
+
+    public String getMethod() {
+        return httpExchange.getRequestMethod();
     }
 }
