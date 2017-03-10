84e18e5fdccd02c631a223b26dd8e83b911531b6
XI317311 <ismael.cabanas@extern.isban.com>
2017-03-10 10:33:26 +0100

    Equals and hashcode implementation

--- src/main/java/cabanas/garcia/ismael/opportunity/http/Parameter.java
+++ src/main/java/cabanas/garcia/ismael/opportunity/http/Parameter.java
@@ -1,12 +1,14 @@
 package cabanas.garcia.ismael.opportunity.http;
 
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
 
 @Builder
 @Getter
 @ToString
+@EqualsAndHashCode
 public class Parameter {
     private String name;
     private String value;
