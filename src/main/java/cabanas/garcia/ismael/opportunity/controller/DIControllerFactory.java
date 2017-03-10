66c16466706b228e838e3c0e0128e3b946222ed5
XI317311 <ismael.cabanas@extern.isban.com>
2017-03-10 15:49:08 +0100

    Updated dependencies injection

--- src/main/java/cabanas/garcia/ismael/opportunity/controller/DIControllerFactory.java
+++ src/main/java/cabanas/garcia/ismael/opportunity/controller/DIControllerFactory.java
@@ -2,6 +2,7 @@ package cabanas.garcia.ismael.opportunity.controller;
 
 import cabanas.garcia.ismael.opportunity.internal.creation.instance.InstantiationException;
 import cabanas.garcia.ismael.opportunity.internal.creation.instance.Instantiator;
+import cabanas.garcia.ismael.opportunity.repository.DefaultUserRepository;
 import cabanas.garcia.ismael.opportunity.service.DefaultUserService;
 import cabanas.garcia.ismael.opportunity.service.UserService;
 import lombok.extern.slf4j.Slf4j;
@@ -21,7 +22,7 @@ public class DIControllerFactory {
         if(clazz.getName().equals(LoginPostController.class.getName())
                 || clazz.getName().equals(UserCreateController.class.getName())){
             try {
-                return clazz.getConstructor(UserService.class).newInstance(new DefaultUserService());
+                return clazz.getConstructor(UserService.class).newInstance(new DefaultUserService(new DefaultUserRepository()));
             } catch (java.lang.InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
                 throw new InstantiationException("Unable to create instance of \'" + clazz.getSimpleName() + "\'.\nPlease ensure it has 0-arg constructor which invokes cleanly.", e);
             }
