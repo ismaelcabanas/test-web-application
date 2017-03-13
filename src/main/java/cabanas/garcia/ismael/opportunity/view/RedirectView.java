4531422da9593881f453aa3a6cc83a7590934e03
XI317311 <ismael.cabanas@extern.isban.com>
2017-03-13 22:22:31 +0100

    New constructor passing redirect path

--- src/main/java/cabanas/garcia/ismael/opportunity/view/RedirectView.java
+++ src/main/java/cabanas/garcia/ismael/opportunity/view/RedirectView.java
@@ -6,8 +6,17 @@ import cabanas.garcia.ismael.opportunity.http.imp.DefaultResponse;
 import java.net.HttpURLConnection;
 
 public class RedirectView implements View {
+    private final String redirectPath;
+
+    public RedirectView(String redirectPath) {
+        this.redirectPath = redirectPath;
+    }
+
     @Override
     public Response render() {
-        return DefaultResponse.builder().statusCode(HttpURLConnection.HTTP_MOVED_TEMP).build();
+        return DefaultResponse.builder()
+                .statusCode(HttpURLConnection.HTTP_MOVED_TEMP)
+                .redirectPath(redirectPath)
+                .build();
     }
 }
