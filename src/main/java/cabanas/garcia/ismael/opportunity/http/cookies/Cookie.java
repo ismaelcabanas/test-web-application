573289195100ab82cbe6b19b1fed03277af2b761
XI317311 <ismael.cabanas@extern.isban.com>
2017-03-16 17:23:58 +0100

    Deleted toString lombok annotation

--- src/main/java/cabanas/garcia/ismael/opportunity/http/cookies/Cookie.java
+++ src/main/java/cabanas/garcia/ismael/opportunity/http/cookies/Cookie.java
@@ -3,12 +3,10 @@ package cabanas.garcia.ismael.opportunity.http.cookies;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.ToString;
 
 @Builder
 @Getter
 @EqualsAndHashCode
-@ToString
 public class Cookie {
     public static final String SESSION_TOKEN = "sessionToken";
 
