11d7876589935ab44ff876684266b518a6b5782c
XI317311 <ismael.cabanas@extern.isban.com>
2017-03-16 17:11:27 +0100

    Added login link to views

--- src/main/java/cabanas/garcia/ismael/opportunity/view/UnAuthorizedRawView.java
+++ src/main/java/cabanas/garcia/ismael/opportunity/view/UnAuthorizedRawView.java
@@ -6,11 +6,20 @@ import cabanas.garcia.ismael.opportunity.http.imp.DefaultResponse;
 import java.net.HttpURLConnection;
 
 public class UnAuthorizedRawView implements WebView{
+
+    String RAW_CONTENT = "<html>\n" +
+            "\t<body>\n" +
+            "\t<h1>Unauthorized!!</h1>\n" +
+            "\t<a href=\"/login\">Login\n" +
+            "\t</a>\n" +
+            "\t</body>\n" +
+            "</html>";
+
     @Override
     public Response render() {
         return DefaultResponse.builder()
                 .statusCode(HttpURLConnection.HTTP_UNAUTHORIZED)
-                .content("Unauthorized user!!!".getBytes())
+                .content(RAW_CONTENT.getBytes())
                 .build();
     }
 
