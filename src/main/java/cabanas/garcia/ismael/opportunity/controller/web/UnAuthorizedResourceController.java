6bcdcca937f5852d0afe50bf149210546d771fc4
XI317311 <ismael.cabanas@extern.isban.com>
2017-03-16 10:50:17 +0100

    Render fobidden view

--- src/main/java/cabanas/garcia/ismael/opportunity/controller/web/UnAuthorizedResourceController.java
+++ src/main/java/cabanas/garcia/ismael/opportunity/controller/web/UnAuthorizedResourceController.java
@@ -3,13 +3,14 @@ package cabanas.garcia.ismael.opportunity.controller.web;
 import cabanas.garcia.ismael.opportunity.controller.Controller;
 import cabanas.garcia.ismael.opportunity.http.Request;
 import cabanas.garcia.ismael.opportunity.support.Resource;
+import cabanas.garcia.ismael.opportunity.view.ForbiddenRawView;
 import cabanas.garcia.ismael.opportunity.view.UnAuthorizedRawView;
 import cabanas.garcia.ismael.opportunity.view.View;
 
 public class UnAuthorizedResourceController extends Controller{
     @Override
     public View process(Request request) {
-        return new UnAuthorizedRawView();
+        return new ForbiddenRawView();
     }
 
     @Override
