2d0278a203644ccbb29125ac8ccaed28f8010bcb
XI317311 <ismael.cabanas@extern.isban.com>
2017-03-16 08:39:26 +0100

    Controller for unauthorized request

---
+++ src/main/java/cabanas/garcia/ismael/opportunity/controller/web/UnAuthorizedResourceController.java
@@ -0,0 +1,19 @@
+package cabanas.garcia.ismael.opportunity.controller.web;
+
+import cabanas.garcia.ismael.opportunity.controller.Controller;
+import cabanas.garcia.ismael.opportunity.http.Request;
+import cabanas.garcia.ismael.opportunity.support.Resource;
+import cabanas.garcia.ismael.opportunity.view.UnAuthorizedRawView;
+import cabanas.garcia.ismael.opportunity.view.View;
+
+public class UnAuthorizedResourceController extends Controller{
+    @Override
+    public View process(Request request) {
+        return new UnAuthorizedRawView();
+    }
+
+    @Override
+    public Resource getMappingPath() {
+        return Resource.builder().path("/forbidden").build();
+    }
+}
