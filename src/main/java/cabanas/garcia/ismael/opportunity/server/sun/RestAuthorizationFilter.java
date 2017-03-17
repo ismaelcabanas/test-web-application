87de9ea8a262c3b30b41ea2b7d91fa18d4d6d5d8
XI317311 <ismael.cabanas@extern.isban.com>
2017-03-17 13:01:12 +0100

    Rest authorization filter

---
+++ src/main/java/cabanas/garcia/ismael/opportunity/server/sun/RestAuthorizationFilter.java
@@ -0,0 +1,72 @@
+package cabanas.garcia.ismael.opportunity.server.sun;
+
+import cabanas.garcia.ismael.opportunity.http.Principal;
+import cabanas.garcia.ismael.opportunity.http.Request;
+import cabanas.garcia.ismael.opportunity.http.RequestFactory;
+import cabanas.garcia.ismael.opportunity.http.RequestMethodEnum;
+import cabanas.garcia.ismael.opportunity.model.User;
+import cabanas.garcia.ismael.opportunity.security.permission.PermissionChecker;
+import cabanas.garcia.ismael.opportunity.service.UserService;
+import cabanas.garcia.ismael.opportunity.util.HttpExchangeUtil;
+import com.sun.net.httpserver.Filter;
+import com.sun.net.httpserver.HttpExchange;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@Slf4j
+public class RestAuthorizationFilter extends Filter{
+
+    private static final String REST_AUTHORIZATION_FILTER_DESCRIPTION = "REST_AUTHORIZATION_FILTER";
+    private final UserService userService;
+    private final PermissionChecker permissionChecker;
+
+    public RestAuthorizationFilter(UserService userService, PermissionChecker permissionChecker) {
+        this.userService = userService;
+        this.permissionChecker = permissionChecker;
+    }
+
+    @Override
+    public void doFilter(HttpExchange httpExchange, Chain chain) throws IOException {
+
+        Request request = RequestFactory.create(httpExchange);
+
+        log.debug("Handle request {}", request);
+
+        Principal principal = request.getPrincipal();
+
+        if(principal != null){
+            log.debug("User {} authenticated", principal.getName());
+
+            if(!request.getMethod().equals(RequestMethodEnum.GET)) {
+                Optional<User> user = userService.findByUsername(principal.getName());
+                if(user.isPresent()){
+                    if(permissionChecker.hasPermission(user.get(), request.getResource())){
+                        chain.doFilter(httpExchange);
+                    }
+                    else{
+                        HttpExchangeUtil.forbidden(httpExchange);
+                    }
+                }
+                else{
+                    log.debug("There is principal but no user found, then unauthorizated user");
+                    HttpExchangeUtil.unauthorized(httpExchange);
+                }
+            }
+            else {
+                chain.doFilter(httpExchange);
+            }
+        }
+        else{
+            log.debug("No user authenticated");
+            HttpExchangeUtil.unauthorized(httpExchange);
+            httpExchange.close();
+        }
+    }
+
+    @Override
+    public String description() {
+        return REST_AUTHORIZATION_FILTER_DESCRIPTION;
+    }
+}
---
+++ src/test/java/cabanas/garcia/ismael/opportunity/server/sun/RestAuthorizationFilterTest.java
@@ -0,0 +1,216 @@
+package cabanas.garcia.ismael.opportunity.server.sun;
+
+import cabanas.garcia.ismael.opportunity.http.Request;
+import cabanas.garcia.ismael.opportunity.http.RequestFactory;
+import cabanas.garcia.ismael.opportunity.model.Role;
+import cabanas.garcia.ismael.opportunity.model.Roles;
+import cabanas.garcia.ismael.opportunity.model.User;
+import cabanas.garcia.ismael.opportunity.security.permission.PermissionChecker;
+import cabanas.garcia.ismael.opportunity.server.sun.stubs.*;
+import cabanas.garcia.ismael.opportunity.service.UserService;
+import com.sun.net.httpserver.Filter;
+import com.sun.net.httpserver.HttpExchange;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.net.HttpURLConnection;
+import java.util.Arrays;
+import java.util.Optional;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class RestAuthorizationFilterTest {
+
+    public static final String USER_1 = "user1";
+    public static final String USERS_PATH = "/users";
+    private static final Roles ROLES_ADMIN = Roles.builder().roleList(Arrays.asList(Role.ADMIN)).build();
+    private static final Roles ROLES_PAGE1 = Roles.builder().roleList(Arrays.asList(Role.builder().name("Page1").build())).build();
+    private static final Optional<User> ADMIN_USER = Optional.of(User.builder().username(USER_1).password("****").roles(ROLES_ADMIN).build());
+    private static final Optional<User> NO_ADMIN_USER = Optional.of(User.builder().username(USER_1).password("****").roles(ROLES_PAGE1).build());
+
+    @Mock
+    private Filter.Chain chain;
+
+    @Mock
+    private UserService userService;
+
+    @Mock
+    private PermissionChecker permissionChecker;
+
+    @Test
+    public void should_filter_doChain_GET_requests_if_authenticated_user() throws Exception{
+        // given
+        HttpExchange httpExchange = new HttpExchangeRestGetWithPrincipalResourceStub(USERS_PATH, USER_1);
+
+        Request request = RequestFactory.create(httpExchange);
+
+        RestAuthorizationFilter sut = new RestAuthorizationFilter(userService, permissionChecker);
+
+        // when
+        sut.doFilter(httpExchange, chain);
+
+        // then
+        verify(chain).doFilter(httpExchange);
+        verifyZeroInteractions(userService, permissionChecker);
+    }
+
+    @Test
+    public void should_filter_doChain_POST_requests_if_authenticated_user_has_permissions() throws Exception{
+        // given
+        HttpExchange httpExchange = new HttpExchangeRestPostWithPrincipalResourceStub(USERS_PATH, USER_1);
+
+        Request request = RequestFactory.create(httpExchange);
+
+        RestAuthorizationFilter sut = new RestAuthorizationFilter(userService, permissionChecker);
+
+        when(userService.findByUsername(anyString())).thenReturn(ADMIN_USER);
+        when(permissionChecker.hasPermission(any(), any())).thenReturn(true);
+
+        // when
+        sut.doFilter(httpExchange, chain);
+
+        // then
+        verify(chain).doFilter(httpExchange);
+        verify(userService).findByUsername(USER_1);
+        verify(permissionChecker).hasPermission(any(), any());
+    }
+
+    @Test
+    public void should_filter_doChain_PUT_requests_if_authenticated_user_has_permissions() throws Exception{
+        // given
+        HttpExchange httpExchange = new HttpExchangeRestPutWithPrincipalResourceStub(USERS_PATH, USER_1);
+
+        Request request = RequestFactory.create(httpExchange);
+
+        RestAuthorizationFilter sut = new RestAuthorizationFilter(userService, permissionChecker);
+
+        when(userService.findByUsername(anyString())).thenReturn(ADMIN_USER);
+        when(permissionChecker.hasPermission(any(), any())).thenReturn(true);
+
+        // when
+        sut.doFilter(httpExchange, chain);
+
+        // then
+        verify(chain).doFilter(httpExchange);
+        verify(userService).findByUsername(USER_1);
+        verify(permissionChecker).hasPermission(any(), any());
+    }
+
+    @Test
+    public void should_filter_doChain_DELETE_requests_if_authenticated_user_has_permissions() throws Exception{
+        // given
+        HttpExchange httpExchange = new HttpExchangeRestDeleteWithPrincipalResourceStub(USERS_PATH, USER_1);
+
+        Request request = RequestFactory.create(httpExchange);
+
+        RestAuthorizationFilter sut = new RestAuthorizationFilter(userService, permissionChecker);
+
+        when(userService.findByUsername(anyString())).thenReturn(ADMIN_USER);
+        when(permissionChecker.hasPermission(any(), any())).thenReturn(true);
+
+        // when
+        sut.doFilter(httpExchange, chain);
+
+        // then
+        verify(chain).doFilter(httpExchange);
+        verify(userService).findByUsername(USER_1);
+        verify(permissionChecker).hasPermission(any(), any());
+    }
+
+    @Test
+    public void should_redirect_to_unauthorization_if_not_principal_in_request() throws Exception{
+        // given
+        HttpExchange httpExchange = new HttpExchangeRestWithoutPrincipalResourceStub(USERS_PATH);
+
+        Request request = RequestFactory.create(httpExchange);
+
+        RestAuthorizationFilter sut = new RestAuthorizationFilter(userService, permissionChecker);
+
+        HttpExchange httpExchangeSpy = spy(httpExchange);
+
+        // when
+        sut.doFilter(httpExchangeSpy, chain);
+
+        // then
+        verifyZeroInteractions(chain, userService, permissionChecker);
+        verify(httpExchangeSpy).sendResponseHeaders(HttpURLConnection.HTTP_UNAUTHORIZED, 0);
+    }
+
+    @Test
+    public void should_redirect_to_forbidden_POST_requests_if_authenticated_user_no_has_permissions() throws Exception{
+        // given
+        HttpExchange httpExchange = new HttpExchangeRestPostWithPrincipalResourceStub(USERS_PATH, USER_1);
+
+        Request request = RequestFactory.create(httpExchange);
+
+        RestAuthorizationFilter sut = new RestAuthorizationFilter(userService, permissionChecker);
+
+        when(userService.findByUsername(anyString())).thenReturn(NO_ADMIN_USER);
+        when(permissionChecker.hasPermission(any(), any())).thenReturn(false);
+
+        HttpExchange httpExchangeSpy = spy(httpExchange);
+
+        // when
+        sut.doFilter(httpExchangeSpy, chain);
+
+        // then
+        verifyZeroInteractions(chain);
+        verify(userService).findByUsername(USER_1);
+        verify(permissionChecker).hasPermission(any(), any());
+        verify(httpExchangeSpy).sendResponseHeaders(HttpURLConnection.HTTP_FORBIDDEN, 0);
+    }
+
+    @Test
+    public void should_redirect_to_forbidden_PUT_requests_if_authenticated_user_no_has_permissions() throws Exception{
+        // given
+        HttpExchange httpExchange = new HttpExchangeRestPutWithPrincipalResourceStub(USERS_PATH, USER_1);
+
+        Request request = RequestFactory.create(httpExchange);
+
+        RestAuthorizationFilter sut = new RestAuthorizationFilter(userService, permissionChecker);
+
+        when(userService.findByUsername(anyString())).thenReturn(NO_ADMIN_USER);
+        when(permissionChecker.hasPermission(any(), any())).thenReturn(false);
+
+        HttpExchange httpExchangeSpy = spy(httpExchange);
+
+        // when
+        sut.doFilter(httpExchangeSpy, chain);
+
+        // then
+        verifyZeroInteractions(chain);
+        verify(userService).findByUsername(USER_1);
+        verify(permissionChecker).hasPermission(any(), any());
+        verify(httpExchangeSpy).sendResponseHeaders(HttpURLConnection.HTTP_FORBIDDEN, 0);
+    }
+
+    @Test
+    public void should_redirect_to_forbidden_DELETE_requests_if_authenticated_user_no_has_permissions() throws Exception{
+        // given
+        HttpExchange httpExchange = new HttpExchangeRestDeleteWithPrincipalResourceStub(USERS_PATH, USER_1);
+
+        Request request = RequestFactory.create(httpExchange);
+
+        RestAuthorizationFilter sut = new RestAuthorizationFilter(userService, permissionChecker);
+
+        when(userService.findByUsername(anyString())).thenReturn(NO_ADMIN_USER);
+        when(permissionChecker.hasPermission(any(), any())).thenReturn(false);
+
+        HttpExchange httpExchangeSpy = spy(httpExchange);
+
+        // when
+        sut.doFilter(httpExchangeSpy, chain);
+
+        // then
+        verifyZeroInteractions(chain);
+        verify(userService).findByUsername(USER_1);
+        verify(permissionChecker).hasPermission(any(), any());
+        verify(httpExchangeSpy).sendResponseHeaders(HttpURLConnection.HTTP_FORBIDDEN, 0);
+    }
+
+}
