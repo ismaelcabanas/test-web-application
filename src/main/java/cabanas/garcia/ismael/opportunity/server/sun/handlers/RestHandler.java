66cab0d7c7c19069c894934464d8c00d82fab107
XI317311 <ismael.cabanas@extern.isban.com>
2017-03-17 13:34:54 +0100

    Trying reducing complexity

--- src/main/java/cabanas/garcia/ismael/opportunity/server/sun/handlers/RestHandler.java
+++ src/main/java/cabanas/garcia/ismael/opportunity/server/sun/handlers/RestHandler.java
@@ -2,15 +2,18 @@ package cabanas.garcia.ismael.opportunity.server.sun.handlers;
 
 import cabanas.garcia.ismael.opportunity.controller.Controller;
 import cabanas.garcia.ismael.opportunity.controller.Controllers;
-import cabanas.garcia.ismael.opportunity.http.*;
+import cabanas.garcia.ismael.opportunity.http.Principal;
+import cabanas.garcia.ismael.opportunity.http.Request;
+import cabanas.garcia.ismael.opportunity.http.RequestFactory;
+import cabanas.garcia.ismael.opportunity.http.Response;
 import cabanas.garcia.ismael.opportunity.model.User;
 import cabanas.garcia.ismael.opportunity.security.permission.PermissionChecker;
 import cabanas.garcia.ismael.opportunity.server.sun.AbstractHttpHandler;
 import cabanas.garcia.ismael.opportunity.service.UserService;
+import cabanas.garcia.ismael.opportunity.support.Resource;
 import cabanas.garcia.ismael.opportunity.util.HttpExchangeUtil;
 import cabanas.garcia.ismael.opportunity.view.View;
 import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
@@ -37,51 +40,48 @@ public class RestHandler extends AbstractHttpHandler{
 
         Principal principal = request.getPrincipal();
 
-        /*if(authorized(request)){
-            Controller controller = controllers.select(request);
-
-            View view = controller.process(request);
-
-            Response response = view.render();
-
-            process(httpExchange, request, response);
-        }*/
-        if(principal != null){
-            log.debug("User {} authenticated", principal.getName());
-
-            Optional<User> user = userService.findByUsername(principal.getName());
-            if(user.isPresent()){
-                if(permissionChecker.hasPermission(user.get(), request.getResource())){
-                    Controller controller = controllers.select(request);
-
-                    View view = controller.process(request);
-
-                    Response response = view.render();
-
-                    process(httpExchange, request, response);
-                }
-                else{
-                    HttpExchangeUtil.forbidden(httpExchange);
-                }
-            }
-            else{
-                log.debug("There is principal but no user found, then unauthorizated user");
-                HttpExchangeUtil.unauthorized(httpExchange);
-            }
-        }
-        else{
-            log.debug("No user authenticated");
+        if(!authenticated(request)){
+            log.debug("There is principal but no user found, then unauthorizated user");
             HttpExchangeUtil.unauthorized(httpExchange);
+            return;
         }
+
+        Optional<User> user = userService.findByUsername(principal.getName());
+
+        if(!authorized(user, request.getResource())){
+            log.debug("User {} not authorized", request.getPrincipal().getName());
+            HttpExchangeUtil.forbidden(httpExchange);
+            return;
+        }
+
+        Controller controller = controllers.select(request);
+
+        View view = controller.process(request);
+
+        Response response = view.render();
+
+        process(httpExchange, request, response);
     }
-/*
-    private boolean authorized(Request request) {
+
+    private boolean authenticated(final Request request) {
         return existPrincipal(request) && existUser(request);
     }
 
-    private boolean existPrincipal(Request request) {
-        return false;
-    }*/
+    private boolean authorized(final Optional<User> user, final Resource resource) {
+        return user.isPresent() && hasPermissions(user.get(), resource);
+    }
+
+    private boolean hasPermissions(final User user, final Resource resource) {
+        return permissionChecker.hasPermission(user, resource);
+    }
+
+    private boolean existUser(final Request request) {
+        return userService.findByUsername(request.getPrincipal().getName()).isPresent();
+    }
+
+    private boolean existPrincipal(final Request request) {
+        return request.getPrincipal() != null;
+    }
 
     private void process(HttpExchange httpExchange, Request request, Response response) throws IOException {
         HttpExchangeUtil.write(httpExchange, response.getStatusCode(), "text/html", response.getContent());
